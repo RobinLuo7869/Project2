d6950e9d6e51d0efb5d786b919e3c69c4d162d06
RobinLuo7869 <59627776+RobinLuo7869@users.noreply.github.com>
2020-03-03 09:39:25 -0500

    Create p5_testing1.js

---
+++ p5_testing1.js
@@ -0,0 +1,59 @@
+var portName = '/dev/cu.usbmodem14201';
+var options = {
+    baudRate: 9600
+};
+var serial;
+var rectColor = 100;
+var rectSize = 100;
+
+var canvas;
+var h1;
+var p;
+var button;
+var input;
+var nameF;
+
+var data;
+
+function setup() {
+    canvas = createCanvas(300, 20);
+    canvas.position(50, 620)
+    h1 = createElement('h3', 'Water Level');
+    h1.position(50, 550);
+    text = p = createP("testing");
+    p.position(120, 580);
+
+    button = createButton("Change")
+    button.position(50, 660);
+
+    input = createInput('type something')
+    input.position(160, 660);
+
+
+    serial = new p5.SerialPort();
+    serial.open(portName, options);
+    serial.on('data', serialEvent);
+    serial.on('error', serialError);
+
+}
+
+
+
+function draw() {
+    background("#000000");
+    fill("#44AAFF")
+    noStroke();
+    rect(0, 0, rectSize, 40);
+
+}
+
+
+function serialError(err) {
+    console.log('Something went wrong with the serial port. ' + err);
+}
+
+function serialEvent() {
+    var data = serial.read();
+    console.log(data， data);
+    rectSize = data;
+}
